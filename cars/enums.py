4ee42b6737d2ca3e8376f88997edb019297a8f74
Filip EonD <filip.chrzan@eond.eu>
2021-05-01 14:31:15 +0200

    create cars

---
+++ cars/enums.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class MaxPassengerCapacityChoices(models.IntegerChoices):
+    ONE = 1
+    THREE = 3
+    FOUR = 4
+    SIX = 6
+    EIGHT = 8
+
+
+class CarClassChoices(models.TextChoices):
+    FIRST = 'FRST', _('First class')
+    BUSINESS = 'BSNS', _('Business class')
+    ECONOMY = 'ECON', _('Economy class')
